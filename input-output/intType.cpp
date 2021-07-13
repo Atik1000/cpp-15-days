130c16c5ae14dd3fdf2d432b07636eb48b8e9d36
Atik1000 <foysalkazi45@gmail.com>
2021-07-14 01:50:42 +0600

    int data type basic problem solving added

---
+++ input-output/intType.cpp
@@ -0,0 +1,252 @@
+// Q:Hello world print
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     cout<<"Hello world !"<<endl;
+//     return 0;
+// }
+//  Q: I Love bangladesh print 3 lines
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     cout<<" I \n love \n Bangladesh \n";
+//     return 0;
+
+// }
+
+// Q: input a value the  print it
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     cout<<a;
+//     return 0;
+// }
+
+// Q: input two value and print sum
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<a+b;
+//     return 0;
+// }
+// Q: input two value and print sum,mul,div
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b<<endl<<a%b<<endl;
+//     return 0;
+// }
+
+// Q: suppose you daily going to school everage s step .if step you d distance ,so what is the total distance  in your school.
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int s,d;
+//     cin>>s>>d;
+//     cout<<s*d<<"cm"<<endl;
+//     return 0;
+// }
+
+// Q:Total marks in 3 subject
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//  int n1,n2,n3;
+//  cin>>n1>>n2>>n3;
+//  cout<<n1+n2+n3<<endl;
+//  return 0;
+// }
+
+// Q: Total amount in 10 person
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,total=0;
+//     cin>>t1>>t2>>t3>>t4>>t5>>t6>>t7>>t8>>t9>>t10;
+//     total=t1+t2+t3+t4+t5+t6+t7+t8+t9+t10;
+//     cout<<total;
+// }
+
+// Q:convert hexadecimal
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int input;
+//     cin>>input;
+//     printf("%x\n",input);
+//     return 0;
+// }
+
+// Q:print 7 days
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     cout<<"Saturday"<<endl<<"Sunday"<<endl<<"Monday"<<endl<<"Wednesday"<<endl<<"Thursday"<<endl<<"Friday"<<endl;
+// }
+
+// Q:print 12 month
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     cout<<"January"<<endl<<"February"<<endl<<"March"<<endl<<"April"<<endl<<"May"<<endl<<"June"<<endl<<"July"<<endl<<"August"<<endl<<"september"<<endl<<"October"<<endl<<"November"<<endl<<"December \n";
+// }
+
+// Q:print your name,father name,mothername
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     cout<<"Kazi Atik Foysal"<<endl<<"A T M Latifur Rahaman"<<endl<<"Afroza khatun"<<endl;
+
+//     }
+
+// Q:Total exam and pen
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//    int exam,pen;
+//    cin>>exam;
+//    pen=exam;
+//    cout<<pen;
+
+//     }
+
+// Q:broken egg
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//    int e,b;
+//    cin>>e>>b;
+//    cout<<e-b;
+
+//     }
+
+// Q:week and day
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int day, week;
+//     cin >> day;
+//     week = day / 7;
+
+//     cout << week << "\n";
+// }
+
+// Q: week in tv serail
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int W,totalTvSeries;
+//     cin >> W;
+//     totalTvSeries=W*3;
+
+//     cout << totalTvSeries << "\n";
+// }
+
+// Q:Apple weight
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int numOfApple,appleEvgWeight,totalappleWeight;
+//     cin >> numOfApple>>appleEvgWeight;
+//     totalappleWeight=numOfApple*appleEvgWeight;
+//     cout << totalappleWeight << "\n";
+// }
+
+// Q: num of friend and number of chocklate
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int numOfChocklate,numOfFriend;
+//     float westChocklate;
+//     cin>>numOfChocklate>>numOfFriend;
+
+//     westChocklate=numOfChocklate%numOfFriend;
+//     cout << westChocklate << "\n";
+//     return 0;
+// }
+
+// Q:count of chocklate
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int numOfChocklate,numOfFriend;
+//     float westChocklate;
+//     cin>>numOfChocklate>>numOfFriend;
+
+//     westChocklate=numOfChocklate/numOfFriend;
+//     cout << westChocklate << "\n";
+//     return 0;
+// }
+
+// Q:convert octal
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int number;
+//     cin>>number;
+//     printf("%o\n",number);
+// }
+
+// Q:sum of number
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int num,temp,sum=0,r;
+//     cin>>num;
+//     temp=num;
+//     while(temp!=0){
+// r=temp%10
+//         sum=sum+r;
+//          temp=temp/10;
+
+//     }
+//     cout<<sum;
+// }
+
+// Q:reverse number
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int num,temp,sum=0,r;
+//     cin>>num;
+//     temp=num;
+//     while(temp!=0){
+//         // 123
+//         r=temp%10; // 123%10=3===12 %10=2===1%%10=1;
+//         sum=sum*10+r;// 0+3;===3*10+2=32===32*10+1=321
+//         temp=temp/10;//12===1
+//     }
+//     cout<<sum;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int i,n,value,sum=0;
+//     cin>>n;
+//     for ( i = 0; i < n; i++)
+//     {
+//        cin>>value;
+//         sum=sum+value;
+//     }
+//     cout<<sum;
+
+// }
