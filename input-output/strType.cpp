23d7fc9f903e0f6da474976ee75179e76bbf3043
Atik1000 <foysalkazi45@gmail.com>
2021-07-15 00:29:18 +0600

    str type added

---
+++ input-output/strType.cpp
@@ -0,0 +1,100 @@
+// Q: print 5 student xm number
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a,b,c,d,e;
+//     cin >> a >> b >> c >> d >> e;
+//     printf(" %4d \n %4d \n %4d \n %4d \n %4d \n ",a,b,c,d,e);
+// }
+
+//     *
+//    ***
+//   *****
+//  *******
+//   *****
+//    ***
+//     *
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     printf("   *   \n");
+//     printf("  ***  \n");
+//     printf(" ***** \n");
+//     printf("*******\n");
+//     printf(" ***** \n");
+//     printf("  ***  \n");
+//     printf("   *   \n");
+// }
+// Q: print ascii value
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+// char c;
+// cin >> c;
+// printf("%d",c);
+// return 0;
+// }
+
+// Q:print capital latter
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     char c;
+//     cin >> c;
+//     printf("%c\n",c-'a'+'A');
+//     return 0;
+// }
+
+// Q:input ascii value and print character sign
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+// int c;
+// cin >> c;
+// printf("%c",c);
+// return 0;
+// }
+
+// Q:print small latter
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     char c;
+//     cin >> c;
+//     printf("%c\n",c-'A'+'a');
+//     return 0;
+// }
+
+//     A
+//    A A
+//   AAAAA
+// A       A
+// A       A
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     printf("    A    \n");
+//     printf("   A A   \n");
+//     printf("  AAAAA  \n");
+//     printf(" A      A \n");
+//     printf(" A      A \n");
+// }
+
+// Q: sum of 3 digit 
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int num; 
+//     cin >> num;
+
+//     cout<<num/100+num/10%10+num%10;
+// }
